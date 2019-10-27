cc4242aecb907aaecc71045ffb673cba08f47231
凉凉 <xyqfer@gmail.com>
2019-10-27 10:51:57 +0800

    Revert "test vp9"
    This reverts commit 9cf82b20c5fcbe1b691064e675ee320f60cbce85.

--- routes/youtube/utils.js
+++ routes/youtube/utils.js
@@ -8,14 +8,7 @@ const getUrl = async (id) => {
         url = YOUTUBE_MAP[id].url;
     } else {
         const { formats } = await ytdl.getBasicInfo(id);
-        
-        try {
-            url = formats.find((item) => {
-                return item.type.includes('vp9') && item.quality_label === '720p';
-            }).url;
-        } catch(err) {
-            // url = formats[0].url;
-        }
+        url = formats[0].url;
         const expire = (new URL(url)).searchParams.get('expire') * 1000;
 
         YOUTUBE_MAP[id] = {
