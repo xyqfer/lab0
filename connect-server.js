7f684d60e032bebfe9857ea92e4f63eaba5038b2
凉凉 <xyqfer@gmail.com>
2019-11-10 00:05:27 +0800

    test

--- connect-server.js
+++ connect-server.js
@@ -44,11 +44,11 @@ var server = net.createServer(function (socket) {
             if (str.includes('-core-engine-cell-')) {
                 socket.end('HTTP/1.1 200 OK\r\n\r\n');
             } else {
-                // socket.write('HTTP/1.1 200 OK\r\n\r\n');
+                socket.write('HTTP/1.1 200 OK\r\n\r\n');
                 // socket.end('xxxxxxxxxxxx');
                 let s = '';
                 for (let i = 1; i <= 65536; i++) {
-                    s += 'x';
+                    s += (i % 10) + '';
                 }
                 console.log('write to ');
                 socket.write(s);
